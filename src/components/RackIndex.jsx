86089ee328404707bf11c56314f9b817ae96561f
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-05 12:56:25 -0600

    basic rack index action

--- src/components/RackIndex.jsx
+++ src/components/RackIndex.jsx
@@ -1,9 +1,34 @@
-
+import axios from "axios"
+import { useState } from "react"
 
 export function RackIndex() {
+  const [racks, setRacks] = useState([])
+
+  const getRacks = async () => {
+    console.log("getRacks");
+    try {
+      const response = await axios.get("http://localhost:3000/bike_racks.json");
+      console.log(response.data);
+      setRacks(response.data);
+    } catch (error) {
+      console.error("Error fetching racks:", error);
+    }
+  };
+
   return (
     <div>
       <h2>Rack Index</h2>
+      <button onClick={getRacks}>Get Racks</button>
+      {racks.map((rack) => (
+        <div key={rack.id}>
+          <h3>Name/Neighborhood: {rack.name}</h3>
+          <p>description: {rack.description}</p>
+          <p>quantity: {rack.quantity}</p>
+          <p>style: {rack.style}</p>
+          <p>latitude: {rack.latitude}</p>
+          <p>longitude: {rack.longitude}</p>
+        </div>
+      ))}
     </div>
   )
 }
