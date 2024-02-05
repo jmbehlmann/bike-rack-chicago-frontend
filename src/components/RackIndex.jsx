20c30e7f8e72c426620ac69e4a2d811101187bfc
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-05 13:08:36 -0600

    frontend index by location

--- src/components/RackIndex.jsx
+++ src/components/RackIndex.jsx
@@ -3,11 +3,12 @@ import { useState } from "react"
 
 export function RackIndex() {
   const [racks, setRacks] = useState([])
+  const [searchLocation, setSearchLocation] = useState("")
 
   const getRacks = async () => {
     console.log("getRacks");
     try {
-      const response = await axios.get("http://localhost:3000/bike_racks.json");
+      const response = await axios.get(`http://localhost:3000/bike_racks.json?location=${searchLocation}`);
       console.log(response.data);
       setRacks(response.data);
     } catch (error) {
@@ -18,10 +19,12 @@ export function RackIndex() {
   return (
     <div>
       <h2>Rack Index</h2>
+      <p>Enter an Address: <input type="text" value={searchLocation} onChange={(event) => setSearchLocation(event.target.value) }/></p>
       <button onClick={getRacks}>Get Racks</button>
+      <h3>the 5 bike racks closest to {searchLocation}</h3>
       {racks.map((rack) => (
         <div key={rack.id}>
-          <h3>Name/Neighborhood: {rack.name}</h3>
+          <h4>Name/Neighborhood: {rack.name}</h4>
           <p>description: {rack.description}</p>
           <p>quantity: {rack.quantity}</p>
           <p>style: {rack.style}</p>
