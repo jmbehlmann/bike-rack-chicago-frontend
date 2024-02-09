28fb401fc0be224419a77e1be77fa1f8662f9c7f
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-09 17:16:23 -0600

    new build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export function Header() {
     <div className="container-xxl">
       <div className="row px-2 mt-2">
         <div className="col-sm-1">
-          <img src="./bike-rack-chicago-frontend/build/star-text.jpg" className="d-block" style={{ height: "2.25rem", objectFit: "contain" }}/>
+          <img src="https://raw.githubusercontent.com/jmbehlmann/bike-rack-chicago-frontend/main/public/star-text.png" className="d-block" style={{ height: "2.25rem", objectFit: "contain" }}/>
         </div>
         <div className="col sm-11 align-left">
           {/* <h3>Bike Rack Chicago</h3> */}
