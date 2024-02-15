3accc69ea6b6cbbefbc70363d629043af3f32ea1
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-15 15:04:52 -0600

    modified about alert

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
   const handleAboutClick = () => {
-    alert('This site provides applications using data that has been modified for use from its original source, www.cityofchicago.org, the official website of the City of Chicago.  The City of Chicago makes no claims as to the content, accuracy, timeliness, or completeness of any of the data provided at this site.  The data provided at this site is subject to change at any time.  It is understood that the data provided at this site is being used at one’s own risk. For questions or concerns please contact me through github');
+    alert('This site was built using following technologies: Ruby, Rails, React, Google Maps, Bootstrap, City of Chicago data, Heroku, Github Pages. This site provides applications using data that has been modified for use from its original source, www.cityofchicago.org, the official website of the City of Chicago.  The City of Chicago makes no claims as to the content, accuracy, timeliness, or completeness of any of the data provided at this site.  The data provided at this site is subject to change at any time.  It is understood that the data provided at this site is being used at one’s own risk. For questions or concerns please contact me through github');
   };
 
   return (
